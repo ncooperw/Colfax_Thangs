68d23e74e91decb3517d307c9e3e8591f940f506
Megan Evans <megan.evans.ducky@gmail.com>
2018-07-14 15:59:58 -0600

    bug

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -68,7 +68,7 @@ $(document).ready(function () {
         },
         // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
         signInFlow: 'popup',
-        signInSuccessUrl: 'index.html',
+        signInSuccessUrl: 'index1.html',
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
